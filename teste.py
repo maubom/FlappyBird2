af398bff222a963b83cd6fa61788f67903dd855a
maubom <maubom@gmail.com>
2022-09-29 14:33:20 -0300

    agar.io

--- teste.py
+++ teste.py
@@ -1,33 +1,107 @@
 import pygame
+import random
 
 pygame.init()
-window = pygame.display.set_mode((300, 300))
+tela = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
 
-rect = pygame.Rect(0, 0, 20, 20)
-rect.center = window.get_rect().center
-vel = 5
+rect = pygame.Rect(0, 0, 15, 15)
+rect.center = tela.get_rect().center
+vel = 1
 
-run = True
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            print(pygame.key.name(event.key))
+class Jogador:
+   def __init__(self, x, y):
+       self.x = x
+       self.y = y
+       self.raio = 6
+       self.cor = (255, 255, 255)
 
-    keys = pygame.key.get_pressed()
+class Bola:
+   def __init__(self, x, y):
+       self.x = x
+       self.y = y
+       self.raio = 5
+       self.cor = (255, 255, 255)
+def main():
+    bolas = []
+    jogadores = []
+    conta = 0
+    jogadores.append(Jogador(random.randint(7, tela.get_width()-7), random.randint(7, tela.get_height()-7)))
+    for i in range(0, random.randint(30, 150)):
+        bolas.append(Bola(random.randint(6, tela.get_width()-6), random.randint(6, tela.get_height()-6)))
+        bolas[i].cor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    populacao = len(bolas)
+    run = True
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                print(pygame.key.name(event.key))
 
-    rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
-    rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * vel
+        x = pygame.mouse.get_pos()[0]
+        y = pygame.mouse.get_pos()[1]
 
-    rect.centerx = rect.centerx % window.get_width()
-    rect.centery = rect.centery % window.get_height()
+        if rect.x > x:
+            rect.x -= vel
+        if rect.x < x:
+            rect.x += vel
+        if rect.y > y:
+            rect.y -= vel
+        if rect.y < y:
+            rect.y += vel
 
-    window.fill(0)
-    pygame.draw.rect(window, (255, 0, 0), rect)
-    pygame.display.flip()
+        if rect.x > tela.get_width() - rect.width:
+            rect.x = tela.get_width() - rect.width
+        if rect.x < 0:
+            rect.x = 0
+        if rect.y > tela.get_height() - rect.width:
+            rect.y = tela.get_height() - rect.width
+        if rect.y < 0:
+            rect.y = 0
 
-pygame.quit()
-exit()
+
+        #rect.h += 1
+        #rect.w += 1
+
+
+
+#        pygame.draw.circle(tela, jogadores[0].cor, (jogadores[0].x, jogadores[0].y), jogadores[0].raio)
+        bolas_remove = []
+
+        for i, bola in enumerate(bolas):
+            if rect.w <= tela.get_width() / 2:
+                if bola.x - bola.raio >= rect.x and bola.x + bola.raio <= rect.x + rect.width  \
+                   and bola.y - bola.raio >= rect.y and bola.y + bola.raio <= rect.y + rect.height:
+                    rect.h +=1
+                    rect.w +=1
+                    bolas_remove.append(i)
+                    conta +=1
+
+        for n in bolas_remove:
+            print(n, len(bolas))
+            if n < len(bolas):
+                bolas.pop(n)
+
+
+
+        if conta > 10:
+            conta = 0
+            for i in range(0, populacao - len(bolas)):
+                bolas.append(Bola(random.randint(6, tela.get_width() - 6), random.randint(6, tela.get_height() - 6)))
+                bolas[i].cor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        tela.fill(0)
+        for bola in bolas:
+            pygame.draw.circle(tela, bola.cor, (bola.x, bola.y), bola.raio)
+
+        pygame.draw.rect(tela, (0, 255, 0), rect)
+        pygame.mouse.set_visible(False)
+        pygame.display.flip()
+
+    pygame.quit()
+    exit()
+
+if __name__ == '__main__':
+    main()
